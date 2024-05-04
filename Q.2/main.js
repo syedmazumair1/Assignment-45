6c42e1ca52c4c718ac2833d25e7946513460f371
[syedmazumai1] <[syedumair374@gmail.com]>
2024-05-05 01:24:50 +0500

    first commit

---
+++ Q.2/main.js
@@ -0,0 +1,4 @@
+// storing the person name in a veriable
+var personName = "mazia";
+// printing a message to the person 
+console.log("Hello ".concat(personName, " would you like to learn some python today?"));
---
+++ Q.2/main.ts
@@ -0,0 +1,7 @@
+// storing the person name in a veriable
+
+let personName: string = "mazia";
+
+// printing a message to the person 
+
+console.log(`Hello ${personName} would you like to learn some python today?`);
